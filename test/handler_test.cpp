7fc830df19824e180fb45956c2e00b55e4a44cdf
Ryan Peterman <ryanlpeterman@gmail.com>
2017-02-22 15:13:34 -0800

    add echo test, add 404 test for handler

--- test/handler_test.cpp
+++ test/handler_test.cpp
@@ -2,20 +2,38 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <memory>
 
 #include "gtest/gtest.h"
 #include "../handler.h"
 
 
-// TEST(EchoHandlerTest, EchoClassTest) {
-//   std::string str = "Hello";
-//   std::vector<char>body(str.begin(), str.end());
+TEST(EchoHandlerTest, EchoClassTest) {
+  std::unique_ptr<EchoHandler> handler(new EchoHandler);
+  std::string str_req = "GET /echo HTTP/1.1\r\ntest\r\n\r\n";
 
-//   echo_handler e(body);
-//   http_response r = e.handle_request();
+  auto req = Request::Parse(str_req);
 
-//   EXPECT_EQ(r.to_string(), "HTTP/1.1 200 OK\r\nContent-Length: 1\r\nContent-Type: text/plain\r\n\r\nHello");
-// }
+  Response res;
+  RequestHandler::Status s = handler->HandleRequest(*req, &res);
+
+  std::string expected = "HTTP/1.1 200 OK\r\nContent-Length: " + std::to_string(str_req.size() - 4) +"\r\nContent-Type: text/plain\r\n\r\n" + str_req;
+  EXPECT_EQ(res.ToString(), expected);
+  EXPECT_EQ(s, RequestHandler::PASS);
+}
+
+TEST(NotFoundHandlerTest, NotFoundClassTest) {
+  std::unique_ptr<NotFoundHandler> handler(new NotFoundHandler);
+  Request req;
+  Response res;
+  RequestHandler::Status s = handler->HandleRequest(req, &res);
+
+  std::string body = "404 NOT FOUND";
+  std::string expected = "HTTP/1.1 404 Not found\r\nContent-Length: " + std::to_string(body.size()) +"\r\nContent-Type: text/plain\r\n\r\n" + body;
+  EXPECT_EQ(res.ToString(), expected);
+  EXPECT_EQ(s, RequestHandler::PASS);
+}
+
 
 // TEST(StaticHandlerTest, StaticClassTest){
 //   std::string url = "/public/index.html";
@@ -24,6 +42,4 @@
 //   http_response r = handle.handle_request();
 //   EXPECT_EQ(r.to_string(), "HTTP/1.1 200 OK\r\nContent-Length: 28\r\nContent-Type: text/html\r\n\r\n<head> Hello World! </head>\n");
 // }
-
-
 
